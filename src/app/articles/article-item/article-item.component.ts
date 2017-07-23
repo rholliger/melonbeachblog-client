9be8b7b5e39ea5febbc76134d696e89c6b49d09c
Roy Holliger <roy@holliger.li>
2017-07-23 16:26:45 +0200

    Added articles table

--- src/app/articles/article-item/article-item.component.ts
+++ src/app/articles/article-item/article-item.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../article.model';
 
 @Component({
-  selector: 'app-article-item',
+  selector: '[app-article-item]',
   templateUrl: './article-item.component.html',
   styleUrls: ['./article-item.component.scss']
 })
